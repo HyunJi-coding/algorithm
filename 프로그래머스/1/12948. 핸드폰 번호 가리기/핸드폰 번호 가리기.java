e86ea0ba8791c19b2cb40a81c11b776b1c7964b6
HyunJi-coding <71685092+HyunJi-coding@users.noreply.github.com>
2024-08-15 22:02:47 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 3.23 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) {
+        int length = phone_number.length();
+        String hidden = "*".repeat(length - 4);
+        String lastFourDigits = phone_number.substring(length - 4);
+        
+        return hidden + lastFourDigits;
+    }
+    
+}
