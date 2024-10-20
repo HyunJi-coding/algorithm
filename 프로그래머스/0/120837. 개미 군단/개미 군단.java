155a6e4f093154eb877b17e1ba520e12e249c596
HyunJi-coding <71685092+HyunJi-coding@users.noreply.github.com>
2024-10-20 21:45:55 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int hp) {
+        int[] ants = {5, 3, 1}; 
+        int count = 0;
+
+        for (int ant : ants) {
+            count += hp / ant; 
+            hp %= ant; 
+        }
+
+        return count; 
+    }
+}
