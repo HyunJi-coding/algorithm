0dd86dcc26e5697f51e9fe68f26c5df75bc25e32
HyunJi-coding <71685092+HyunJi-coding@users.noreply.github.com>
2024-11-28 23:47:47 +0900

    [level 3] Title: 이중우선순위큐, Time: 73.35 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (String operation : operations) {
+            String[] parts = operation.split(" ");
+            String command = parts[0];
+            int number = Integer.parseInt(parts[1]);
+
+            if (command.equals("I")) {
+                minHeap.add(number);
+                maxHeap.add(number);
+            } else if (command.equals("D")) {
+                if (minHeap.isEmpty()) continue;
+                if (number == 1) {
+                    int max = maxHeap.poll();
+                    minHeap.remove(max);
+                } else {
+                    int min = minHeap.poll();
+                    maxHeap.remove(min);
+                }
+            }
+        }
+
+        if (minHeap.isEmpty()) return new int[]{0, 0};
+        return new int[]{maxHeap.peek(), minHeap.peek()};
+    }
+}
